640b73cb55e43ad40d5186d67457d559091062ed
dioppapa <94880876+dioppapa@users.noreply.github.com>
2021-12-07 02:51:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,66 @@ app.get('/' , function ( req , res) {
 
 });
 
+app.post('/stripet', function(req, res) {
+  
+
+  
+ // console.log(' allhamdoulilah sant yallah waayy ma //recommencer ')
+
+  
+    
+})
+app.post('/purchase', function(req, res) {
+  fs.readFile('items.json', function(error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      const itemsJson = JSON.parse(data)
+      const itemsArray = itemsJson.music.concat(itemsJson.merch)
+      let total = 0
+      req.body.items.forEach(function(item) {
+        const itemJson = itemsArray.find(function(i) {
+          return i.id == item.id
+        })
+        total = total + itemJson.price * item.quantity
+      })
+
+      stripe.charges.create({
+        amount: total,
+        source: req.body.stripeTokenId,
+        currency: 'usd'
+      }).then(function() {
+        console.log('Charge Successful')
+        res.json({ message: 'Successfully purchased items' })
+      }).catch(function() {
+        console.log('Charge Fail')
+        res.status(500).end()
+      })
+    }
+  })
+})
+
+
+app.post('/stripe', function(req, res) {
+  
+  stripe.charges.create({
+
+    amount: 10000,
+    source: req.body.stripeTokenId,
+    currency: 'usd'
+
+  }).then(function() {
+    console.log('Charge Successful allhamodoulilah')
+   
+  }).catch(function() {
+    console.log('Charge Fail')
+    
+  })
+
+  
+    
+})
+
 
     app.listen(port, function(){
         console.log("Listening on port : " + port); //it work
