c4be0e192d841136ad310ae9cd202d976a6d936c
dioppapa <94880876+dioppapa@users.noreply.github.com>
2021-12-07 02:45:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ var port = process.env.PORT || 8080;
 var todoCont = require('./routes/controller') ;
 require('dotenv').config()
 const session = require("express-session");
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+
+const stripe = require('stripe')(stripeSecretKey)
 
 
 
 app.set('view engine' , 'ejs') ;
 app.use('/public' , express.static('public')) ;
+app.use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: false,
+    
+    })
+  );
+
 
 todoCont(app);
 
